3f4e8cf4bc6a71957ae78a05294c66fca4cd9974
Benjamin Henke <benjaminmhenke@gmail.com>
2021-07-30 15:21:32 -0500

    Our groups code

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,69 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log("jquery is loaded!")
+  console.log("jquery is loaded!");
+  $('#submitButton').on('click',addBets);
 }
+let playerNumber = 1;
+
+
+
+function addBets() {
+    console.log('inside addBets');
+
+  let roundOfBets = {
+        bet1: $('#playeroneInput').val(),
+        bet2: $('#playertwoInput').val(),
+        bet3: $('#playerthreeInput').val()
+    };
+    console.log('roundOfBets is', roundOfBets);
+    $.ajax({
+        method: 'POST',
+        url: '/addBets',
+        data: roundOfBets,
+    }).then((response) => {
+        console.log('POST /addBets', response);
+
+      getResults();
+        
+    }).catch(error => {
+        console.log('POST / server failed', error);
+        $('body').append(`
+        <h2> 
+            Failed to save bets! Check your inputs!
+        </h2>
+        `);
+    });
+    $('#playeroneInput').val(''),
+    $('#playertwoInput').val(''),
+    $('#playerthreeInput').val('')
+
+}
+
+function getResults() {
+  $.ajax({
+      method: 'GET',
+      url: '/betResults',
+  })
+      .then((response) => {
+          console.log('GET /betResults response', response)
+        let resultsList =  $('#results');
+        console.log('results list element', resultsList);
+
+        resultsList.empty();
+        for (let result of response) {
+          console.log(result);
+            resultsList.append(`
+              <h1> Player ${playerNumber}'s number ${result} </h1>
+            `);
+            playerNumber++;
+        }
+      }
+)
+
+playerNumber = 1;
+
+
+};
+
+
--- server/server.js
+++ server/server.js
@@ -3,6 +3,14 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+//
+let randomNum = getRandom(1,25);
+// let randomNum = 15;
+console.log(randomNum);
+
+let betArray = [];
+let resultArray = [];
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -10,8 +18,68 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+// app.get('/numberGenerated', function(req, res) {
+  
+// }
+app.post('/addBets', (req, res) => {
+  console.log('got new bets submitted')
+  let playeroneBet = req.body.bet1;
+  let playertwoBet = req.body.bet2;
+  let playerthreeBet = req.body.bet3; 
+  console.log(req.body);
+
+  // clear out old data
+  betArray = [];
+  resultArray = [];
+  betArray.push({p1Bet: playeroneBet, p2Bet: playertwoBet, p3Bet: playerthreeBet});
+
+  console.log("bet array is", betArray);
+  console.log("bet array at 0 dot p1bet", (betArray[0].p1Bet));
+  iteratethruBets();
+  res.sendStatus(201); // 
+});
+
+//send back 
+// }
+app.get('/betResults', (req, res) => {
+  console.log('got bet results back')
+  res.send(resultArray);
+});
+
+
+
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+function iteratethruBets() {
+  resultArray.push(compareBets(betArray[0].p1Bet));
+  resultArray.push(compareBets(betArray[0].p2Bet));
+  resultArray.push(compareBets(betArray[0].p3Bet));
+  console.log("random num is:", randomNum );
+  console.log(resultArray);
+}
+
+function compareBets(inputNum) {
+  if (inputNum == randomNum) {
+    randomNum = getRandom(1,25);
+    return `${inputNum} is the correct answer`;
+    console.log(`${inputNum} is the correct answer`);
+  } 
+  else if (inputNum > randomNum) {
+    return `${inputNum} is higher`;
+    console.log(`${inputNum} is higher`);
+  } 
+  else if (inputNum < randomNum) {
+    return `${inputNum} is lower`;
+    console.log(`${inputNum} is lower`);
+  } 
+
+}
 
 
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
+  console.log ('Server is running on port', PORT);
+});
